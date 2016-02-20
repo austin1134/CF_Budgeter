1bac33a5a08360182fb0b8fc1ae96f6fe0237ecb
austin1134 <austint1>
2016-02-19 19:31:10 -0500

    Added more functionality and tweaks to accounts and households

---
+++ Controllers/BudgetItemsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CF_Budgeter.Models;
+
+namespace CF_Budgeter.Controllers
+{
+    public class BudgetItemsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: BudgetItems
+        public async Task<ActionResult> Index()
+        {
+            var budgetItems = db.BudgetItems.Include(b => b.Category);
+            return View(await budgetItems.ToListAsync());
+        }
+
+        // GET: BudgetItems/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BudgetItem budgetItem = await db.BudgetItems.FindAsync(id);
+            if (budgetItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(budgetItem);
+        }
+
+        // GET: BudgetItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.BudgetId = new SelectList(db.Budgets, "Id", "Name");
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: BudgetItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,CategoryId,Amount,BudgetId")] BudgetItem budgetItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BudgetItems.Add(budgetItem);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", budgetItem.CategoryId);
+            return View(budgetItem);
+        }
+
+        // GET: BudgetItems/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BudgetItem budgetItem = await db.BudgetItems.FindAsync(id);
+            if (budgetItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", budgetItem.CategoryId);
+            return View(budgetItem);
+        }
+
+        // POST: BudgetItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,CategoryId,Amount,BudgetId")] BudgetItem budgetItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(budgetItem).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", budgetItem.CategoryId);
+            return View(budgetItem);
+        }
+
+        // GET: BudgetItems/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BudgetItem budgetItem = await db.BudgetItems.FindAsync(id);
+            if (budgetItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(budgetItem);
+        }
+
+        // POST: BudgetItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            BudgetItem budgetItem = await db.BudgetItems.FindAsync(id);
+            db.BudgetItems.Remove(budgetItem);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
